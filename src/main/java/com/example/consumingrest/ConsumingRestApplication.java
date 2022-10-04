8c536028bf63e52393185b4eb4cdc6cca0bba689
gschipper <gschipper@gmail.com>
2022-10-04 22:13:58 +0200

    commenting the purpose of the beans

--- src/main/java/com/example/consumingrest/ConsumingRestApplication.java
+++ src/main/java/com/example/consumingrest/ConsumingRestApplication.java
@@ -12,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 public class ConsumingRestApplication {
 
+    // To send output to the log (the console, in this example)
     public static final Logger log = LoggerFactory.getLogger(ConsumingRestApplication.class);
 
     public static void main(String[] args) {
@@ -19,17 +20,20 @@ public class ConsumingRestApplication {
     }
 
     @Bean
+    // Uses the Jackson JSON processing library to process the incoming data
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder.build();
     }
 
     @Bean
-    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+    // Runs the RestTemplate (and, consequently, fetches our quotation) on startup
+    public CommandLineRunner runner(RestTemplate restTemplate) throws Exception {
         return args -> {
             Quote quote = restTemplate.getForObject(
                     "https://gistcdn.githack.com/ayan-b/ff0441b5a8d6c489b58659ffb849aff4/raw/e1c5ca10f7bea57edd793c4189ea8339de534b45/response.json",
                     Quote.class
             );
+            // logging the mapped response
             log.info(quote.toString());
         };
     }
